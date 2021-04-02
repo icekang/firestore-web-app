8be6916aeb80e61bddb38870cc6b034985b34449
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2021-04-02 21:22:24 +0700

    Un-minified script

--- inclass-activity/public/script.js
+++ inclass-activity/public/script.js
@@ -11,52 +11,57 @@ const db = firebase.firestore();
 const items_ref = db.collection("items");
 
 async function showItemsInTable() {
-  const e = document.getElementById("main-table-body");
-  e.innerHTML = "";
-  (await items_ref.get()).docs
-      .map((e) => ({ docId: e.id, ...e.data() }))
-      .map((t) => {
-        e.innerHTML += `
-        <tr id="${t.docId}">
+  const table_body = document.getElementById('main-table-body')
+  table_body.innerHTML = ''
+
+  const collection = await items_ref.get()
+  const items = collection.map((item) => ({ docId: item.id, ...item.data() }))
+  items.map((item) => {
+      table_body.innerHTML += `
+      <tr id="${t.docId}">
           <td>${t.name}</td>
           <td>${t.owner}</td>
           <td>${t.price}</td>
           <td><button class="delete-row" onclick="deleteItem('${t.docId}')">ลบ</button></td>
-        </tr>`;
-      });
-}
+        </tr>
+      `
+  })
 
 document.addEventListener("DOMContentLoaded", function (e) {
   console.log("showing items from database"), showItemsInTable();
 });
+
 function redrawDOM() {
-  window.document.dispatchEvent(
-    new Event("DOMContentLoaded", { bubbles: !0, cancelable: !0 })
-  ),
-    (document.getElementById("item-to-add").value = ""),
-    (document.getElementById("name-to-add").value = "0"),
-    (document.getElementById("price-to-add").value = "");
+  window.document.dispatchEvent(new Event("DOMContentLoaded", {
+      bubbles: true,
+      cancelable: true
+  }));
+  document.getElementById("item-to-add").value = "";
+  document.getElementById("name-to-add").value = "0";
+  document.getElementById("price-to-add").value = "";
 }
 
 async function addItem() {
-  const e = document.getElementById("item-to-add").value,
-    t = document.getElementById("name-to-add").value,
-    d = document.getElementById("price-to-add").value;
-  items_ref.add({ name: e, owner: t, price: d });
+  const name = document.getElementById("item-to-add").value
+  const owner = document.getElementById("name-to-add").value
+  const price = document.getElementById("price-to-add").value
+  items_ref.add({ name: name, owner: owner, price: price });
 }
 
 var addButton = document.querySelector("#add-newrow");
-async function deleteItem(e) {
-  console.log("delete the item with", e),
-    items_ref
-      .doc(`${e}`)
-      .delete()
-      .then(() => {
-        redrawDOM();
-      });
-}
 addButton.onclick = async () => {
   addItem().then(() => {
     redrawDOM();
   });
-};
+}
+
+async function deleteItem(docId) {
+  console.log("delete the item with", docId)
+  items_ref
+    .doc(`${docId}`)
+    .delete()
+    .then(() => {
+      redrawDOM();
+    });
+}
+
